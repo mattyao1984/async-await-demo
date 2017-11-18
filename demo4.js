e660baa30b137fc5bee5d72e58873e0effc32958
Matt Yao <mattyao1984@gmail.com>
2017-11-18 13:13:03 +1100

    Handle errors

---
+++ demo4.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+
+async function showGithubUser(handle) {
+  const url = `https://api.github.com/users/${handle}`;
+  const response = await fetch(url);
+  const body = await response.json();
+
+  if(response.status !== 200) {
+    throw Error(body.message);
+  }
+
+  return body;
+}
+
+async function fetchGithubUser(handle) {
+  try {
+    const user = await showGithubUser(handle);
+    console.log('user: ', user);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+fetchGithubUser('Idontexist-OK');
